093edbc2e8364e0b8afcd0d719e003d6843c7edd
aria-may-saunders <aria.saunders@yahoo.com>
2024-12-28 17:00:38 +0000

    Added player to screen

--- main.py
+++ main.py
@@ -3,11 +3,14 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -15,7 +18,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill((0, 0, 0))
+        player.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick() / 1000
